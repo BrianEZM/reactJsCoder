ee69ac353ba049806a498a812f9533fea5e9eb2f
Brian_Zapata_Marin <brianezm@gmail.com>
2022-03-21 19:27:44 -0300

    desafio navbar en reactjs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Desafios from './Desafios.js';
 
-function App() {
+function App(props) {
+  
+  console.log(props.name);
+
   return (
     <div className="App">
+      <nav className="navbar1">
+          <a className='marca1'>Marca RANDOM</a>
+          <a href='#'>Inicio</a>
+          <a href='#'>Productos</a>
+          <a href='#'>Quienes somos</a>
+          <a href='#'>Contacto</a>
+          <a href='#'>Perfil</a>
+        </nav>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
+        <section className='card1'> 
+      
+        </section>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Keep moving forward!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );

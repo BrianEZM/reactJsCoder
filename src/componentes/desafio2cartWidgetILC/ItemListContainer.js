53e8ddd8cf7c7fa93689d95650159a878f5b6d07
Brian_Zapata_Marin <brianezm@gmail.com>
2022-03-29 00:53:19 -0300

    agregando useEffect y promesa1

--- src/componentes/desafio2cartWidgetILC/ItemListContainer.js
+++ src/componentes/desafio2cartWidgetILC/ItemListContainer.js
@@ -1,13 +1,46 @@
+import {useEffect, useState} from 'react';
+
+const productosIniciales = [
+    {name: "Tatuaje brazo", precio: 5000, stock: 7},
+    {name: "Tatuaje pierna", precio: 6000, stock: 5},
+    {name: "Tatuaje espalda", precio: 7000, stock: 3},
+    {name: "Tatuaje pequeño", precio: 3000, stock: 10},
+    
+];
+
+
+// USAR MAP PARA RENDERIZAR EL ARRAY "PRODUCTOS" (KEY, ID)
+// DAR MENSAJE AL USUARIO MIENTRAS SE RENDERIZA EL ARRAY "PRODUCTOS"
+
+const promesa1 = new Promise ((res, rej) => {
+    setTimeout(() => {
+    res(productosIniciales)
+},2000)
+
+});
 
 export const ItemListContainer = ({name}) => {
 
-    // var nombre1 = prompt('¿cual es tu nombre');
-    
+    const [productos, setProductos] = useState([]);
 
+
+    useEffect(() => { 
+        
+        promesa1.then((productos) => {
+        setProductos(productos);
+        console.log(promesa1);
+        })
+        .catch(() => {
+            console.log("No funcionó");
+        })
+    
+    }, []);
+    
+    
     return (
         <>
             <h1> {name} </h1>
-            
+
         </>
     );
 };

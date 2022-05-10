3170cc8dd7ed0ec82973a848cd28035abf5f92d6
Brian_Zapata_Marin <brianezm@gmail.com>
2022-05-10 15:26:04 -0300

    fixiado ruteo en ilc & idc, next step ITEM COUNT, CART Y CONTEXT

--- src/componentes/ItemDetailContainer/ItemDetailContainer.js
+++ src/componentes/ItemDetailContainer/ItemDetailContainer.js
@@ -1,70 +1,50 @@
 import { ItemDetail } from "./ItemDetail/ItemDetail";
 import {useEffect, useState} from 'react';
 import { useParams } from "react-router-dom";
+import { getDoc, doc, collection } from "firebase/firestore"
+import { db } from "../../firebase/firebase"
 
-
-const initialProduct = [
-    {name: "Tatuaje brazo", 
-    category: "grandes", 
-    id: 1, 
-    price: 5000,
-    stock: 7, 
-    imag: "/imgs/tatuajeBrazo.jpg",
-    details: "Este es nuestro trabajo mas solicitado, el mismo se puede realizar con distintos diseños y colores. Contactanos para reservar tu turno."},
-
-    {name: "Tatuaje pierna", 
-    category: "medianos", 
-    id: 2, 
-    price: 6000, 
-    stock: 5, 
-    imag: "/imgs/tatuajePierna.jpg",
-    details: "Este es nuestro trabajo mas solicitado, el mismo se puede realizar con distintos diseños y colores. Contactanos para reservar tu turno."},
-
-    {name: "Tatuaje espalda", 
-    category: "pequeños", 
-    id: 3, 
-    price: 7000, 
-    stock: 3, 
-    imag: "/imgs/tatuajeEspalda.jpg",
-    details: "Este es nuestro trabajo mas solicitado, el mismo se puede realizar con distintos diseños y colores. Contactanos para reservar tu turno."},
-];
-
-
-const promesa2 = new Promise ((res, rej) => {
-    setTimeout(() => {
-    res(initialProduct)
-},2000)
-});
 
 export const ItemDetailContainer = () => {
 
     const [product, setProduct] = useState([]);
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
     
     const { id } = useParams();
 
     useEffect(() => {
-        setLoading(true)
-        promesa2.then((product) => {
-
-            if (id) {setProduct(product.find(x => x.id === product.id.toString()))
-            };
-    
+        const productosCollection = collection(db, "productos");
+        const refDoc = doc(productosCollection, id);
+        getDoc(refDoc)
+        .then((result)=>{
+            const refProduct = {
+                id, 
+                ...result.data()
+            }
+            setProduct(refProduct);
         })
-        .catch(() => {
-            console.log("No funcionó");
-        }).finally(() => {
-            setLoading(false)
+        .catch(()=>{
+            setError(true);
         })
+        .finally(()=>{
+            setLoading(false);
+          })
     }, [id]);
 
-
-    return(
+    return (
+        
         <>
-            {loading ? <p>Cargando, es decir ESPERE POR FAVOR</p> : 
-            <ItemDetail product={product}/>
-            };
+            <div className="container">
+                { loading ?
+                    <p>Loading, es decir ESPERE POR FAVOR</p>
+                    : 
+                    error ? 
+                    <h1>Lo sentimos, hubo un error</h1>
+                    :
+                    <ItemDetail product={product} />
+                }
+            </div>
         </>
     );
-
 };
